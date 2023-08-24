607fd204c2eb6daf49d4948df70dfb967c3a7f5a
Wilder <w.cossio.r@gmail.com>
2023-08-24 19:28:26 -0400

    creating the increase effect on the percentage

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let percentege = document.querySelector('.percentage-text');
+let percentegeHtml = document.querySelector('.percentage-text');
+let percentage = 0;
+
+function increasePercentage() {
+    percentage++
+    percentegeHtml.textContent = percentage;
+    console.log(percentage);
+}
+const idInterval = setInterval(increasePercentage, 300);
+
+setTimeout(function () {
+    clearInterval(idInterval);
+}, 900);
 
