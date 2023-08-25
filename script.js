fdf21c2a8693b7e30aec9fecd534b634419adf21
Wilder <w.cossio.r@gmail.com>
2023-08-24 21:29:05 -0400

    decreasing opacity of the percentage text

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ let percentegeText = document.querySelector('.percentage-text');
 let bg = document.querySelector('.container');
 let percentage = 0;
 let blurBg = 40;
+let opacityPercentText = 1.00;
 
 function increasePercentage() {
     percentage++
     blurBg = blurBg - 0.4;
+    opacityPercentText = opacityPercentText - 0.01;
+    console.log(opacityPercentText);
     if (percentage > 99) {
         clearInterval(idInterval);
     }
     percentegeText.textContent = percentage.toString() + '%';
     bg.style.filter = `blur(${blurBg}px)`;
+    percentegeText.style.opacity = `${opacityPercentText}`;
 
 }
 const idInterval = setInterval(increasePercentage, 30);
