132a23dc21b7660bb006d6d9b023ee67a40dcade
Wilder <w.cossio.r@gmail.com>
2023-08-24 21:13:44 -0400

    refactoring clearInterval and decreasing blur

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-let percentegeHtml = document.querySelector('.percentage-text');
+let percentegeText = document.querySelector('.percentage-text');
+let bg = document.querySelector('.container');
 let percentage = 0;
+let blurBg = 40;
 
 function increasePercentage() {
     percentage++
-    let percentageText = percentage.toString();
-    percentegeHtml.textContent = percentageText + '%';
+    blurBg = blurBg - 0.4;
+    if (percentage > 99) {
+        clearInterval(idInterval);
+    }
+    percentegeText.textContent = percentage.toString() + '%';
+    bg.style.filter = `blur(${blurBg}px)`;
+
 }
-const idInterval = setInterval(increasePercentage, 40);
-
-setTimeout(function () {
-    clearInterval(idInterval);
-}, 4000);
-
+const idInterval = setInterval(increasePercentage, 30);
